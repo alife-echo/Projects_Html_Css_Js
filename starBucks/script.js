63133b75550e255920d31bd9319b1f0359d317bc
alife-echo <alifesilva90@gmail.com>
2023-02-17 16:22:37 -0300

    media mobile

--- starBucks/script.js
+++ starBucks/script.js
@@ -1,8 +1,16 @@
+
 document.getElementById('btnMenu').addEventListener('click',(e)=>{
     e.preventDefault()
+    if(window.innerWidth <= 700){
+        document.getElementById('sideBar').style.width = '50%'
+    }
+    else if(window.innerWidth <= 1100){
+        document.getElementById('sideBar').style.width = '30%'
+    }
+    else{
         document.getElementById('sideBar').style.width = '20%'
-        document.getElementById('shadowBar').style.width = '100%'
-       
+    }
+    document.getElementById('shadowBar').style.width = '100%'
 })
 
 let elements = document.querySelectorAll('#sideBar,#shadowBar')
