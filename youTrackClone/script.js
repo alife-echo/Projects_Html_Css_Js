f49580360a4a31375e404a64f8242c33e35af777
alife-echo <alifesilva90@gmail.com>
2023-02-22 21:58:41 -0300

    animações modal

--- youTrackClone/script.js
+++ youTrackClone/script.js
@@ -1,5 +1,7 @@
 
 let btnAdd = document.querySelectorAll('.styleButton')
+let titleTaskInput = document.getElementById('inTaskTitle')
+let taskAreaInput = document.getElementById('inTaskArea')
 for(let i =0; i<btnAdd.length;i++){
     btnAdd[i].addEventListener('click',()=>{
          let modal = document.querySelector('.modal')
@@ -9,4 +11,6 @@ for(let i =0; i<btnAdd.length;i++){
 document.getElementById('btnCancelTask').addEventListener('click',()=>{
     let modal = document.querySelector('.modal')
     modal.classList.remove('showModal')
+    titleTaskInput.value = ''
+    taskAreaInput.value = ''
 })
