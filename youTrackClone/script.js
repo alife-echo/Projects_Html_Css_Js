9436cfb83b5729c075dc310b907c47323e7fded1
alife-echo <alifesilva90@gmail.com>
2023-02-26 20:10:44 -0300

    terminando criação de task

--- youTrackClone/script.js
+++ youTrackClone/script.js
@@ -2,21 +2,66 @@
 let btnAdd = document.querySelectorAll('.styleButton')
 let titleTaskInput = document.getElementById('inTaskTitle')
 let taskAreaInput = document.getElementById('inTaskArea')
+let modal = document.querySelector('.modal')
+let divStage = ''
 for(let i =0; i<btnAdd.length;i++){
     btnAdd[i].addEventListener('click',()=>{
          let modal = document.querySelector('.modal')
          modal.classList.add('showModal')
     })
 }
-document.getElementById('btnCancelTask').addEventListener('click',()=>{
-    let modal = document.querySelector('.modal')
+document.getElementById('btnCancelTask').addEventListener('click',()=>{ 
     modal.classList.remove('showModal')
     titleTaskInput.value = ''
     taskAreaInput.value = ''
 })
-document.getElementById()
 
+let addTaskButtons = document.querySelectorAll('.styleButton');
+addTaskButtons.forEach(function(button) {
+  button.addEventListener('click', function(event) {
 
+    let parentDiv = (event.target.parentNode).parentNode;
+
+    let parentClassName = parentDiv.classList[0];
+    divStage = parentClassName
+    console.log(parentClassName);
+  });
+});
+function createTodo(titleTask,taskTextArea,receiveBox){
+    let createClass = '.' + receiveBox
+    let containerTodo = document.querySelector(createClass)
+    let div = document.createElement('div')
+    let details = document.createElement('details')
+    let sumary = document.createElement('summary')
+    let input = document.createElement('button')
+    let p = document.createElement('p')
+    let img = document.createElement('img')
+    let nodeTextTitle = document.createTextNode(titleTask)
+    let nodeTextArea = document.createTextNode(taskTextArea)
+    input.classList.add('styleButton')
+    div.classList.add('contentTodo')
+    img.src = './imgs/trash.png'
+    img.style.width = '18px'
+    img.style.height = '18px'
+    containerTodo.appendChild(div)
+    input.appendChild(img)
+    p.appendChild(nodeTextArea)
+    sumary.appendChild(nodeTextTitle)
+    details.appendChild(sumary)
+    details.appendChild(p)
+    div.appendChild(details)
+    div.appendChild(input)
+    modal.classList.remove('showModal')
+    titleTaskInput.value = ''
+    taskAreaInput.value = ''   
+}
+
+document.getElementById('btnAdd').addEventListener('click',(e)=>{
+  let inTaskTitleText = document.getElementById("inTaskTitle").value
+  let inTaskTextArea = document.getElementById('inTaskArea').value
+  createTodo(inTaskTitleText,inTaskTextArea,divStage)
+})
+console.log(document.querySelector('.open,.progress,.toVerify,.concluded:last-of-type').offsetHeight)
 /* 
 var divsBoxTodo = document.querySelectorAll('.boxTodo');
 
