8316172d75962e63e16eb216c12e147d00e80ca6
alife-echo <alifesilva90@gmail.com>
2023-02-25 21:09:28 -0300

    criando template contentTodo para adicionar ao js

--- youTrackClone/script.js
+++ youTrackClone/script.js
@@ -14,3 +14,24 @@ document.getElementById('btnCancelTask').addEventListener('click',()=>{
     titleTaskInput.value = ''
     taskAreaInput.value = ''
 })
+document.getElementById()
+
+
+/* 
+var divsBoxTodo = document.querySelectorAll('.boxTodo');
+
+// função para atualizar a altura de todas as divs boxTodo
+function atualizarAlturaBoxTodo() {
+  // obtém a altura do último contentTodo adicionado
+  var alturaContentTodo = document.querySelector('.contentTodo:last-of-type').offsetHeight;
+
+  // atualiza a altura de todas as divs boxTodo com a altura do último contentTodo adicionado
+  for (var i = 0; i < divsBoxTodo.length; i++) {
+    divsBoxTodo[i].style.height = alturaContentTodo + 'px';
+  }
+}
+
+// adiciona um evento onchange para detectar quando um novo contentTodo é adicionado
+var inputContentTodo = document.querySelector('#inputContentTodo');
+inputContentTodo.addEventListener('change', atualizarAlturaBoxTodo);
+*/
