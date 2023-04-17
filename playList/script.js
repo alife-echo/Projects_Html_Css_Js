c5a35e722cc323c6faa48c5d41c1d4317a95a8cb
alife-echo <alifesilva90@gmail.com>
2023-04-16 23:49:03 -0300

    upando imgs,audios e array objetos

---
+++ playList/script.js
@@ -0,0 +1,32 @@
+let musics = [ 
+    {name:'Black Flag Main Theme',author:'Ubisoft Music',srcAudio:'./themes/edwardSong.mp3',srcImg:'./imgs/kenway.png'},
+    {name:'Can You Feel My Heart',author:'Bring Me The Horizon',srcAudio:'./themes/bringMeHorizon.mp3',srcImg:'./imgs/gigachad.jpg'},
+    {name:'Make It Bun Dem',author:'Damian Marley',srcAudio:'./themes/makeItBun.mp3',srcImg:'./imgs/vaas.jpg'},
+    {name:'Do I Wanna Know',author:'Arctic Monkeys',srcAudio:'./themes/doIWannaKnow.mp3',srcImg:'./imgs/alex.jpg'},
+    {name:'Epilogue Main Theme ',author:'Hans Zimmer',srcAudio:'./themes/crisisEpilogue.mp3',srcImg:'./imgs/prophet.jpg'},
+    {name:'Forest Swords',author:'Hood',srcAudio:'./themes/forestSwords.mp3',srcImg:'./imgs/shay.jpg'}
+]
+
+/*
+play(): reproduz o áudio.
+pause(): pausa o áudio.
+currentTime: define ou retorna a posição atual do áudio em segundos.
+duration: retorna a duração total do áudio em segundos.
+*/
+
+document.querySelector("#btnBackMusic").addEventListener('click',()=>{
+})
+
+document.querySelector("#btnStateMusic").addEventListener('click',()=>{
+})
+
+document.querySelector("#btnNextMusic").addEventListener('click',()=>{
+})
+
+document.querySelector("#btnVolumeUp").addEventListener('click',()=>{
+})
+document.querySelector("#btnLoop").addEventListener('click',()=>{
+})
+
+document.querySelector("#btnVolumeDown").addEventListener('click',()=>{
+})
