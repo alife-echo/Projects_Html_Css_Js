b69e97687802fe3195d36d63aea155780afa5cd7
alife-echo <alifesilva90@gmail.com>
2023-04-18 14:11:48 -0300

    list musics playlist

--- playList/script.js
+++ playList/script.js
@@ -6,13 +6,41 @@ let musics = [
     {name:'Epilogue Main Theme ',author:'Hans Zimmer',srcAudio:'./themes/crisisEpilogue.mp3',srcImg:'./imgs/prophet.jpg'},
     {name:'Forest Swords',author:'Hood',srcAudio:'./themes/forestSwords.mp3',srcImg:'./imgs/shay.jpg'}
 ]
-
+//new Audio(url)
 /*
 play(): reproduz o áudio.
 pause(): pausa o áudio.
 currentTime: define ou retorna a posição atual do áudio em segundos.
 duration: retorna a duração total do áudio em segundos.
 */
+window.onload = ()=> 
+{
+let containerMusics = document.querySelector('.playListContainer')
+ musics.map(item => {
+    let imgMusic = document.createElement('img')
+    let itemMusic = document.createElement('div')
+    let infoMusic = document.createElement('div')
+    let titleMusic = document.createElement('p')
+    let authorMusic = document.createElement('p')
+    let textTitle = document.createTextNode(item.name)
+    let textAuthor = document.createTextNode(item.author)
+    itemMusic.className = 'itemMusic'
+    infoMusic.className = 'infoMusic'
+    imgMusic.className = 'itemImg'
+    titleMusic.className = 'titleMusic'
+    authorMusic.className = 'authorMusic'
+    imgMusic.src = item.srcImg
+    itemMusic.appendChild(imgMusic)
+    itemMusic.appendChild(infoMusic)
+    titleMusic.appendChild(textTitle)
+    authorMusic.appendChild(textAuthor)
+    infoMusic.appendChild(titleMusic)
+    infoMusic.appendChild(authorMusic)  
+    containerMusics.appendChild(itemMusic)
+ })
+        
+   
+}
 
 document.querySelector("#btnBackMusic").addEventListener('click',()=>{
 })
